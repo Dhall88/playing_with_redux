ba1ae0cef2fa161956929ebb4a4d744d08ac07f0
David <davidchall388@gmail.com>
2020-09-14 12:27:17 -0400

    added connect to Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/postActions'
 
 class Posts extends Component {
 
-    state ={
-        posts: []
+    componentDidMount() {
+        this.props.fetchPosts();
     }
 
-    componentWillMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => this.setState({posts: data}))
-    }
     render() {
         const postItems = this.state.posts.map(post => (
             <div key = {post.id}>
@@ -27,5 +24,5 @@ class Posts extends Component {
     }
 }
 
-export default Posts;
+export default connect(null, {fetchPosts})(Posts);
 
